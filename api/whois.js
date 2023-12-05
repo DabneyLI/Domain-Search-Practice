915d04f86d3ca2fea55665b036048f8743290061
DabneyLI <144627567+DabneyLI@users.noreply.github.com>
2023-12-05 22:03:05 +0100

    Update whois.js

--- api/whois.js
+++ api/whois.js
@@ -1,16 +1,19 @@
-const fetch = require('node-fetch');
-
 module.exports = async (req, res) => {
     const { name, suffix } = req.query;
     const apiUrl = `https://whois.freeaiapi.xyz/?name=${name}&suffix=${suffix}`;
+
     try {
         const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error(`Failed to fetch WHOIS data: ${response.statusText}`);
-        }
         const data = await response.json();
+        
+        if (!response.ok) {
+            console.error('API response error:', data);
+            throw new Error(`API response not ok: ${response.status}`);
+        }
+
         res.json(data);
     } catch (error) {
+        console.error('Serverless Function error:', error);
         res.status(500).json({ error: error.message });
     }
 };
